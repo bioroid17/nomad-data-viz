983d46ec92c770eec684ea361f41f6f9703dec42
bioroid17 <trace1997@ajou.ac.kr>
2025-05-15 00:26:11 +0900

    Dashboard - Layouts in Dash

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 from dash import Dash, html, dcc
 import plotly.express as px
 
-app = Dash()
+stylesheets = [
+    "https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css",
+    "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap",
+]
 
+app = Dash(__name__, external_stylesheets=stylesheets)
+
+app.layout = html.Div(
+    style={
+        "minHeight": "100vh",
+        "backgroundColor": "#111111",
+        "color": "white",
+        "fontFamily": "Open Sans, sans-serif",
+    },
+    children=[
+        html.Header(
+            style={
+                "textAlign": "center",
+                "paddingTop": "50px",
+            },
+            children=[
+                html.H1(
+                    "Corona Dashboard",
+                    style={"fontSize": 50},
+                )
+            ],
+        )
+    ],
+)
 
 if __name__ == "__main__":
     app.run(debug=True)
