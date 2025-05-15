c43c3a48168f775a1355f768a854435f1011003d
bioroid17 <trace1997@ajou.ac.kr>
2025-05-15 16:47:11 +0900

    Dashboard - Cleaning the Code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dash import Dash, html, dcc
 import plotly.express as px
 from data import country_df
+from builders import make_table
 
 stylesheets = [
     "https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css",
@@ -29,39 +30,7 @@ app.layout = html.Div(
                 )
             ],
         ),
-        html.Div(
-            children=[
-                html.Div(
-                    children=[
-                        html.Table(
-                            children=[
-                                html.Thead(
-                                    children=[
-                                        html.Tr(
-                                            children=[
-                                                html.Th(column_name.replace("_", " "))
-                                                for column_name in country_df.columns
-                                            ]
-                                        )
-                                    ]
-                                ),
-                                html.Tbody(
-                                    children=[
-                                        html.Tr(
-                                            children=[
-                                                html.Td(value_column)
-                                                for value_column in value
-                                            ]
-                                        )
-                                        for value in country_df.values
-                                    ]
-                                ),
-                            ],
-                        )
-                    ]
-                )
-            ]
-        ),
+        html.Div(children=[html.Div(children=[make_table(country_df)])]),
     ],
 )
 
