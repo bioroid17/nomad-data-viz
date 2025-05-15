fc2ea68a9f5675f34cb2996b435a0f1d832a58d4
bioroid17 <trace1997@ajou.ac.kr>
2025-05-15 15:04:45 +0900

    Dashboard - Countries Table part 1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dash import Dash, html, dcc
 import plotly.express as px
+from data import country_df
 
 stylesheets = [
     "https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css",
@@ -27,7 +28,40 @@ app.layout = html.Div(
                     style={"fontSize": 50},
                 )
             ],
-        )
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Table(
+                            children=[
+                                html.Thead(
+                                    children=[
+                                        html.Tr(
+                                            children=[
+                                                html.Th(column_name.replace("_", " "))
+                                                for column_name in country_df.columns
+                                            ]
+                                        )
+                                    ]
+                                ),
+                                html.Tbody(
+                                    children=[
+                                        html.Tr(
+                                            children=[
+                                                html.Td(value_column)
+                                                for value_column in value
+                                            ]
+                                        )
+                                        for value in country_df.values
+                                    ]
+                                ),
+                            ],
+                        )
+                    ]
+                )
+            ]
+        ),
     ],
 )
 
